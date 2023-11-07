2e8a621e6a9d2c768386a2a0eee5e65f56e69705
sudorituraj <riturajsingh.dev@gmail.com>
2023-11-07 23:24:59 +0530

    my first commit in software worldgit status Kudus to me Cc @ritanshoo

--- src/app/page.js
+++ src/app/page.js
@@ -1,95 +1,43 @@
+"use client"; 
+import React, { useState } from 'react'
 import Image from 'next/image'
 import styles from './page.module.css'
 
 export default function Home() {
+  const dataURL = "https://jsonplaceholder.typicode.com/todos/";
+
+  const [data, setdata] = useState(null);
+
+  const getData = async () => {
+    try {
+      fetch(dataURL)
+        .then(response => response.json())
+        .then(json => setdata(json))
+    } catch (error) {
+
+    }
+  }
   return (
     <main className={styles.main}>
       <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>src/app/page.js</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
+        {/* {JSON.stringify(data)} */}
       </div>
 
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
+      <div>
+      <h2>Todo List</h2>
+      <ul>
+        {data?.map((todo) => (
+          <li key={todo.id}>
+            <p><strong>Task:</strong> {todo.title}</p>
+            <p><strong>Completed:</strong> {todo.completed ? 'Yes' : 'No'}</p>
+          </li>
+        ))}
+      </ul>
+    </div>
 
-      <div className={styles.grid}>
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore the Next.js 13 playground.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
+      <button onClick={() => getData()}>
+        get data
+      </button>
     </main>
   )
 }
